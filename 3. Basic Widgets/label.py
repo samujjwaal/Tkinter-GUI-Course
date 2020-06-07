fec353288fd0307c22e52e4acb5144e8f017d3bb
Samujjwaal Dey <sdey9@uic.edu>
2020-06-07 00:02:24 -0500

    Implement image label

--- 3. Basic Widgets/label.py
+++ 3. Basic Widgets/label.py
@@ -6,14 +6,20 @@ root = tk.Tk()
 label = ttk.Label(
     root, text="Samujjwaal here, working using tkinter python GUI library"
 )
+# attach label to root window
+label.pack()
+
 # wrap and center align label text
-label.config(wraplength=100, justify=tk.CENTER)
+label.config(wraplength=100, justify="center")
 # modify color of text and label
 label.config(foreground="blue", background="yellow")
 # change font of label text
 label.config(font=("Fira Code", 12, "italic"))
 
-label.config()
-label.pack()
+# import image -- only gif images supported
+pic = tk.PhotoImage(file="samujjwaal.gif")
+
+# compound attribute to put image to left of label text
+label.config(image=pic, compound="left")
 
 root.mainloop()
