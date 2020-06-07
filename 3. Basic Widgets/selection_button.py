566670d9f946f67ae67d0d6995977bdbe936a6dd
Samujjwaal Dey <sdey9@uic.edu>
2020-06-07 02:12:51 -0500

    added comments explaination

--- 3. Basic Widgets/selection_button.py
+++ 3. Basic Widgets/selection_button.py
@@ -2,21 +2,24 @@ import tkinter as tk
 from tkinter import ttk
 
 root = tk.Tk()
-
+# define a stringvar object
 var = tk.StringVar()
 var.set("Daft")
 
+# create check button
 check = ttk.Checkbutton(root, text="Daft?")
 check.pack()
 
 
 def whats_var():
+    # method to get current value of stringvar object
     print(var.get())
 
 
+# modify default value returned by checkbutton
 check.config(variable=var, onvalue="Yes", offvalue="No", command=whats_var)
 
-
+# create series of radio buttons
 ttk.Radiobutton(
     root, text="github", variable=var, value="github", command=whats_var
 ).pack()
@@ -30,6 +33,7 @@ ttk.Radiobutton(
     root, text="github", variable=var, value="github", command=whats_var
 ).pack()
 
+# assign current value of stringvar object as text for check button
 check.config(textvariable=var)
 
 root.mainloop()
