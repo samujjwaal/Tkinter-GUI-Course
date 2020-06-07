5e1df5d36460efe3e0e6cb9a85a8f5f390af6f1d
Samujjwaal Dey <sdey9@uic.edu>
2020-06-07 03:41:43 -0500

    Implement spinbox

--- 3. Basic Widgets/selection_box.py
+++ 3. Basic Widgets/selection_box.py
@@ -22,4 +22,16 @@ def what_vehicle(event):
 # binding event to combobox to execute callback
 combobox.bind("<<ComboboxSelected>>", what_vehicle)
 
+
+engine = StringVar()
+
+
+def what_engine():
+    # method invoked when new item selected from combobox
+    print(spinbox.get())
+
+
+spinbox = Spinbox(root, from_=800, to=3000, textvariable=engine, command=what_engine)
+spinbox.pack()
+
 root.mainloop()
