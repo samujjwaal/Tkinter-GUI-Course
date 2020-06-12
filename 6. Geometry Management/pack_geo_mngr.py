e803c88d135d3c024c1514537d37a0dcb44d22d8
Samujjwaal Dey <sdey9@uic.edu>
2020-06-12 18:51:38 -0500

    Pack Geo. - Labels stacked horizontally

--- 6. Geometry Management/pack_geo_mngr.py
+++ 6. Geometry Management/pack_geo_mngr.py
@@ -3,12 +3,17 @@ from tkinter import ttk
 
 root = Tk()
 
-ttk.Label(root, text="One", background="yellow").pack(fill=BOTH,expand=True)
+label = ttk.Label(root, text="One", background="yellow")
+label.pack(side=LEFT, anchor="nw")
 
-ttk.Label(root, text="Two", background="orange").pack(fill=BOTH)
+ttk.Label(root, text="Two", background="orange").pack(side=LEFT, padx=10, pady=10)
 
-ttk.Label(root, text="Three", background="pink").pack(fill=BOTH,expand=True)
+ttk.Label(root, text="Three", background="pink").pack(side=LEFT, ipadx=10, ipady=10)
 
+for widget in root.pack_slaves():
+    widget.pack_configure(fill=BOTH, expand=True)
+    print(widget.pack_info())
 
+label.pack_forget()
 
 root.mainloop()
