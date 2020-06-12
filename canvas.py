0a2dc03396e8a4a36b730bf39a8d095abf7eb847
Samujjwaal Dey <sdey9@uic.edu>
2020-06-11 21:01:16 -0500

    Implement complex shapes in canvas

--- canvas.py
+++ canvas.py
@@ -26,6 +26,42 @@ canvas.itemconfigure(line, splinesteps=5)
 canvas.itemconfigure(line, splinesteps=100)
 
 # to delete line from canvas
-# canvas.delete(line)
+canvas.delete(line)
+
+# create rectangle using coordinates of opposite corners
+rect = canvas.create_rectangle(160, 120, 480, 360)
+canvas.itemconfigure(rect, fill="yellow")
+# create oval by coordinates of opposite corners
+oval = canvas.create_oval(160, 120, 480, 360)
+# create arc, by default 0 to 90 degree
+arc = canvas.create_arc(160, 1, 480, 240)
+# arc fromm 0 to 180 deg
+canvas.itemconfigure(arc, start=0, extent=180, fill="green")
+# create triangle using coordinate pairs
+poly = canvas.create_polygon(160, 360, 320, 480, 480, 360, fill="blue")
+
+# add text to canvas
+text = canvas.create_text(320, 240, text="Python", font=("Courier", 32))
+
+# add image to canvas
+img = PhotoImage(file=".\\3. Basic Widgets\samujjwaal.gif")
+image = canvas.create_image(320, 240, image=img)
+
+# to place text above image
+canvas.lift(text)
+# to move image below
+canvas.lower(image)
+# places image just below text
+canvas.lower(image, text)
+
+button = Button(canvas, text="Button here")
+# add button to canvas
+canvas.create_window(320, 60, window=button)
+
+# add tags to canvas items
+canvas.itemconfigure(rect, tag=["shape"])
+canvas.itemconfigure(oval, tag=["shape", "round"])
+# modify items of a particular tag together
+canvas.itemconfigure("shape", fill="grey")
 
 root.mainloop()
