247f565f88bc56f99cabc7ce33f7563415145216
Samujjwaal Dey <sdey9@uic.edu>
2020-06-12 19:17:57 -0500

    Grid geo. reformat code

--- 6. Geometry Management/grid_geo_mngr.py
+++ 6. Geometry Management/grid_geo_mngr.py
@@ -3,17 +3,25 @@ from tkinter import ttk
 
 root = Tk()
 
-root.rowconfigure(0,weight=1)
-root.rowconfigure(1,weight=3)
+root.rowconfigure(0, weight=1)
+root.rowconfigure(1, weight=3)
 
-root.columnconfigure(2,weight=1)
+root.columnconfigure(2, weight=1)
 
-ttk.Label(root, text="Orange", background="orange").grid(row=0, column=2, rowspan=2,sticky='nsew')
+ttk.Label(root, text="Orange", background="orange").grid(
+    row=0, column=2, rowspan=2, sticky="nsew"
+)
 
-ttk.Label(root, text="Red", background="red").grid(row=1, column=0, columnspan=2,sticky='nsew')
+ttk.Label(root, text="Red", background="red").grid(
+    row=1, column=0, columnspan=2, sticky="nsew"
+)
 
-ttk.Label(root, text="Purple", background="purple").grid(row=0, column=0,sticky='nsew',padx=10,pady=10)
+ttk.Label(root, text="Purple", background="purple").grid(
+    row=0, column=0, sticky="nsew", padx=10, pady=10
+)
 
-ttk.Label(root, text="Green", background="green").grid(row=0, column=1,sticky='nsew',ipadx=25,ipady=25)
+ttk.Label(root, text="Green", background="green").grid(
+    row=0, column=1, sticky="nsew", ipadx=25, ipady=25
+)
 
 root.mainloop()
