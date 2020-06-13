fab89a74b41337ebaa03d6cde163fd8160eb1141
Samujjwaal Dey <sdey9@uic.edu>
2020-06-13 10:10:57 -0500

    Divided single root window into multiple frames

--- 8. Building an Application/feedback_survey.py
+++ 8. Building an Application/feedback_survey.py
@@ -5,30 +5,39 @@ from tkinter import ttk
 class Feedback:
     def __init__(self, master):
 
+        self.info_frame = ttk.Frame(master)
+        self.info_frame.pack()
+
         self.pic = tk.PhotoImage(file="tour_logo.gif")
         self.info_label = ttk.Label(
-            master,
+            self.info_frame,
             text="Explore California Feedback Survey",
             image=self.pic,
             compound="left",
         )
         self.info_label.grid(row=0, column=0, columnspan=2, sticky="nsew")
 
-        ttk.Label(master, text="Name").grid(row=1, column=0, sticky="ew")
-        self.name_entry = ttk.Entry(master)
+        self.fields_frame = ttk.Frame(master)
+        self.fields_frame.pack()
+
+        ttk.Label(self.fields_frame, text="Name").grid(row=1, column=0, sticky="ew")
+        self.name_entry = ttk.Entry(self.fields_frame)
         self.name_entry.grid(row=1, column=1)
 
-        ttk.Label(master, text="Email").grid(row=2, column=0, sticky="ew")
-        self.email_entry = ttk.Entry(master)
+        ttk.Label(self.fields_frame, text="Email").grid(row=2, column=0, sticky="ew")
+        self.email_entry = ttk.Entry(self.fields_frame)
         self.email_entry.grid(row=2, column=1)
 
-        ttk.Label(master, text="Comments").grid(row=3, column=0, sticky="ew")
-        self.comments_text = tk.Text(master, height=4, wrap="word")
+        ttk.Label(self.fields_frame, text="Comments").grid(row=3, column=0, sticky="ew")
+        self.comments_text = tk.Text(self.fields_frame, height=4, wrap="word")
         self.comments_text.grid(row=3, column=1)
 
-        self.submit_button = ttk.Button(master, text="Submit")
+        self.buttons_frame = ttk.Frame(master)
+        self.buttons_frame.pack()
+
+        self.submit_button = ttk.Button(self.buttons_frame, text="Submit")
         self.submit_button.grid(row=4, column=0)
-        self.clear_button = ttk.Button(master, text="Clear")
+        self.clear_button = ttk.Button(self.buttons_frame, text="Clear")
         self.clear_button.grid(row=4, column=1)
 
 
