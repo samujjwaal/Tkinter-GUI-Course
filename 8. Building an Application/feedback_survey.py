d1fc3076606c1abda9c044b385857f744e261e2f
Samujjwaal Dey <sdey9@uic.edu>
2020-06-13 09:12:19 -0500

    Create feedback form from template

---
+++ 8. Building an Application/feedback_survey.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+from tkinter import ttk
+
+
+class Feedback:
+    def __init__(self, master):
+        pass
+
+
+def main():
+
+    root = tk.Tk()
+    feedback = Feedback(root)
+    root.mainloop()
+
+
+if __name__ == "__main__":
+    main()
